03ba3e6e4308e6e78120e43751cf965622c32a11
NgocDuong <phamduongx9@gmail.com>
2022-08-15 23:52:46 +0700

    push code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import queryString from 'query-string';
 import './App.css';
 
 function App() {
+  const [files, setFiles] = useState();
+
+  const handleFiles = (e) => {
+    if (e.target.files[0]) {
+      const filesUp = e.target.files[0];
+      setFiles(filesUp);
+    }
+  }
+
+  const handleSubmit = async () => {
+    const axiosClient = axios.create({
+      paramsSerializer: params => queryString.stringify(params)
+    });
+    const res = await axiosClient.get(`http://localhost:5000/sign-s3?file-name=${files.name}&file-type=${files.type}`);
+    console.log(res.data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input className='app-input' type="file" onChange={handleFiles}></input>
+      <div className='app-btn-submit' onClick={handleSubmit}>Submit</div>
+      <div className='app-img-wrap'>
+        <img src='' alt='' className='app-img'></img>
+      </div>
     </div>
   );
 }
